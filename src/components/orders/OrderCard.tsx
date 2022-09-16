7571fadf2a6abae0427283baf2e93a1949a23a7b
[TanStanPlan] <[tanmanstanplan@gmail.com]>
2022-09-16 16:08:58 -0400

    Finished up a basic OrderCard to find all Orders by a User

--- src/components/orders/OrderCard.tsx
+++ src/components/orders/OrderCard.tsx
@@ -1,3 +1,43 @@
-export default function OrderCard() {
-    
+import { Box, CardContent, Typography, CardActions, Button, Grid } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import { Rating } from '@mui/material';
+import Card from '@mui/material/Card';
+import { useEffect, useState } from "react";
+import Order from "../../models/Order";
+import { apiGetAllUserOrders } from "../../remote/e-commerce-api/orderService";
+
+interface orderProps{
+    order: Order,
+    key: number,
 }
+
+
+export const OrderCard = (props: orderProps) => {
+    const [order, setOrder] = useState<any>();
+
+    useEffect(() =>{
+        async function runEffect(){
+            const response = await apiGetAllUserOrders();
+            console.log(response);
+            setOrder(response.payload)
+        }
+        runEffect();
+    }, []);
+
+    return(
+        <Card sx={{ width: 345, margin: 2, backgroundColor: grey[200] }}>
+      <CardContent>
+        <Typography gutterBottom>
+          {props.order.id}
+        </Typography>
+        <Typography variant="body2">
+            {props.order.orderDate}
+        </Typography>
+        <Typography variant="body2">
+          {props.order.shipmentAddress}
+        </Typography>
+      </CardContent>
+    </Card>
+    )
+}
+    
