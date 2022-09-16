8f7f6c5cb7306115cd12f9c6a7a080943aa4e5b6
Paul Mork <paul.a.mork@gmail.com>
2022-09-16 12:51:12 -0700

    Added AllOrder component and OrderCard component

--- src/components/orders/AllOrders.tsx
+++ src/components/orders/AllOrders.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import Order from '../../models/Order';
+import { apiGetAllUserOrders } from '../../remote/e-commerce-api/orderService';
 
 
 interface allOrdersProps {
@@ -6,12 +9,20 @@ interface allOrdersProps {
     handleNext: () => void
 }
 
-
 export default function AllOrders(props: allOrdersProps){
+    const [orders, setOrder] = useState<Order[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await apiGetAllUserOrders();
+            setOrder(response.payload);
+        }
+        fetchData();
+    }, [])
 
     
 
     return(
+        
         <React.Fragment>
 
 
