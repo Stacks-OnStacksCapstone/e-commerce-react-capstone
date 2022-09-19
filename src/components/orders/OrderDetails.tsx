65c8b0da4fa5f9301d7242195e31810f5c0b1feb
Paul Mork <paul.a.mork@gmail.com>
2022-09-19 09:22:32 -0700

    Added component for displaying order details

--- src/components/orders/OrderDetails.tsx
+++ src/components/orders/OrderDetails.tsx
@@ -1,14 +1,18 @@
-import { CardContent, Typography } from "@material-ui/core";
+import { Box, CardContent, CardMedia, Container, Grid, Typography } from "@material-ui/core";
 import Card from '@mui/material/Card';
 import grey from "@mui/material/colors/grey";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import OrderDetail from "../../models/OrderDetail";
+import Product from "../../models/Product";
 import { apiGetOrderDetails } from "../../remote/e-commerce-api/orderService";
+import { apiGetProductById } from "../../remote/e-commerce-api/productService"
+import OrderProduct from "./OrderProduct";
 
 export const OrderDetails = () => {
    const {id} = useParams();
    const [orderDetailsInfo, setOrderDetailsInfo] = useState<OrderDetail[]>([]);
+   const [productInfo, setProductInfo] = useState<Product>(new Product(0, "", 0, "", 0, ""));
 
    useEffect(() => {
         const fetchResponse = async () => {
@@ -16,24 +20,30 @@ export const OrderDetails = () => {
             console.log(response);
             console.log(id);
             setOrderDetailsInfo(response.payload);
+
         }
         fetchResponse();
    }, [])
 
    return(
     <>
-    <Card sx={{ width: 345, margin: 2, backgroundColor: grey[200] }}>
+    <Container style={{alignItems:"center", justifyContent:"center", marginTop:60}}>
+    <Grid container spacing={2} direction="column" alignItems="center" justifyContent="center">
     {orderDetailsInfo.map((item) => {
         return <>
-        <CardContent>
-            <Typography>OrderDetail ID: {item.id}</Typography>
-            <Typography>Order ID: {item.ordersId}</Typography>
-            <Typography>Product ID: {item.productId}</Typography>
-            <Typography>Quantity: {item.quantity}</Typography>
-        </CardContent>
+            <Card sx={{ width: 345, backgroundColor: grey[200]}}>
+            <CardContent>
+                <Typography>OrderDetail ID: {item.id}</Typography>
+                <Typography>Order ID: {item.ordersId}</Typography>
+                <OrderProduct product = {item.productId} />
+                <Typography>Quantity: {item.quantity}</Typography>
+            </CardContent>
+            </Card>
+            <br />
         </>
     })}
-    </Card>
+    </Grid>
+    </Container>
     </>
    )
 }
