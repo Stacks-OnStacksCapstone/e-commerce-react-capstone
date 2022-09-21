165c5193a68eb92c69ab3d16ce2495f678343a4a
Pitmon Foxall <foxthe4th@gmail.com>
2022-09-21 12:58:32 -0600

    Started adding more detail to user profile appearance.

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -9,7 +9,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import {Container} from "@mui/material";
+import { Container } from "@mui/material";
+import Paper from '@mui/material/Paper';
 
 
 const theme = createTheme();
@@ -73,78 +74,90 @@ export default function UserProfile() {
     }
 
     return (
-            <>
-            <Box color="inherit" sx={{ m: 4}}>
-                    <Typography variant="h2">Welcome to Your Dashboard, {user?.firstName}!</Typography>
+        <>
+            <Box color="inherit" sx={{ m: 4 }}>
+                <Typography variant="h2">Welcome to Your Dashboard, {user?.firstName}!</Typography>
             </Box>
-             
+
+
+
             <Container color="inherit" component="main" maxWidth="xs">
 
-                <Box color="inherit" sx={{ m: 3, mx: "auto"}}>
-                    <Typography variant="h4"> Update Your Profile</Typography>
-                </Box>
+                <Paper elevation={3}>
 
-                <Box color="inherit" component="form" noValidate sx={{ mt: 3 }}>
+                    <Box color="inherit" sx={{ m: 3, mx: "auto" }}>
+                        <Typography variant="h4"> Update Your Profile</Typography>
+                    </Box>
 
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="given-name"
-                                name="firstName"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                value={formData.firstName}
-                                onChange={(event) => setFormData({ ...formData, firstName: event.target.value })}
-                                autoFocus
-                            />
-                        </Grid>
-                        <br />
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="family-name"
-                                value={formData.lastName}
-                                onChange={(event) => setFormData({ ...formData, lastName: event.target.value })}
-                            />
-                        </Grid>
+                    <Box color="inherit" component="form" noValidate sx={{ mt: 3 }}>
 
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                onChange={(event) => setFormData({ ...formData, password: event.target.value })}
-                            />
-                            <Box sx={{ mt: 3, mb: 2, backgroundColor: "#90CAF9"}}>
-                                <Button fullWidth onClick={update}>Update</Button>
-                            </Box>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    value={formData.firstName}
+                                    onChange={(event) => setFormData({ ...formData, firstName: event.target.value })}
+                                    autoFocus
+                                />
+                            </Grid>
+                            <br />
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                    value={formData.lastName}
+                                    onChange={(event) => setFormData({ ...formData, lastName: event.target.value })}
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    onChange={(event) => setFormData({ ...formData, password: event.target.value })}
+                                />
+                                <Box sx={{ mt: 3, mb: 2, backgroundColor: "#90CAF9" }}>
+                                    <Button fullWidth onClick={update}>Update</Button>
+                                </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    {persisted === undefined ? <p></p> : <p>{persisted}</p>}
-                </Box>
+                        {persisted === undefined ? <p></p> : <p>{persisted}</p>}
+                    </Box>
+                </Paper>
             </Container>
+
+
+
 
             <Container color="inherit" maxWidth="xs" >
 
-                <Box color="inherit" sx={{ m: 3, mx: "auto"}}>
-                    <Typography variant="h4"> Deactivate Your Account</Typography>
-                    <Typography> Please proceed with caution! You will be logged out after deactivating your account.</Typography>
-                </Box>
+                <Paper elevation={3}>
 
-                <Box sx={{ mt: 3, mb: 2, backgroundColor: "#90CAF9"}}>
+                    <Box color="inherit" sx={{ m: 3, mx: "auto" }}>
+                        <Typography variant="h4"> Deactivate Your Account</Typography>
+                        <Typography> Please proceed with caution! You will be logged out after deactivating your account.</Typography>
+                    </Box>
+
+                    <Box sx={{ mt: 3, mb: 2, backgroundColor: "#90CAF9" }}>
                         <Button fullWidth onClick={() => deactivateUser()}>Deactivate</Button>
-                </Box>
+                    </Box>
+                </Paper>
             </Container>
+
         </>
     );
 }
