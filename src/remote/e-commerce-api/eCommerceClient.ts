8abf228e731c43addb1794c5a3c45e7e0aefa94e
calebabeck2020 <calebabeck@gmail.com>
2022-09-26 14:53:52 -0400

    1.0.0 RELEASE - First release of Congo using AWS WITH connection to deployed API

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -5,7 +5,7 @@ axios.defaults.adapter = require('axios/lib/adapters/http');
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://ecommercespringcapstone-env.eba-gshjzfrb.us-east-1.elasticbeanstalk.com',
   headers: {
     'Content-Type': 'application/json',
   },
